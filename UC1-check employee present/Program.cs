35d8972cdf1e692dd2c3207ef3006349156a8cfb
reigny98 <reigny2016@gmail.com>
2022-02-27 23:41:51 -1200

    UC1

---
+++ UC1-check employee present/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UC1_check_employee_present
+{
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+            //Constants
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is present");
+
+            }
+        }
+    }
+}
